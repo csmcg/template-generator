49849323f254a3936c06d0d24b68c176e099ddac
Connor McGarty <cmcgarty@uab.edu>
2018-04-17 14:58:01 -0500

    fixed netbeans bullshittery

--- template-generator/src/HeadingTree.java
+++ template-generator/src/HeadingTree.java
@@ -3,12 +3,12 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package UserDoc;
+
 
 /**
  *
  * @author Connor
  */
-public class Heading {
+public class HeadingTree {
     
 }
