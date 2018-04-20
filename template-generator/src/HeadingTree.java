96b18fe4ffd57b16be6f210344f061f74ff9d59e
Connor McGarty <cmcgarty@uab.edu>
2018-04-20 16:02:06 -0500

    added headers to src files, some methods laid out in ScriptEditor

--- template-generator/src/HeadingTree.java
+++ template-generator/src/HeadingTree.java
@@ -1,14 +1,23 @@
+/* 
+ * File: HeadingTree.java
+ * Author: Connor S. McGarty <cmcgarty@uab.edu>
+ * Assignment: template-generator - EE333 Spring 2018
+ * Vers: 2.0.0 04/20/2018 csm - initial coding
+ *
+ * Credits: 
+ */
+
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
 
-
 /**
  *
- * @author Connor
+ * @author Connor S. McGarty <cmcgarty@uab.edu>
  */
 public class HeadingTree {
-    
+
 }
--- template-generator/src/ScriptEditor.java
+++ template-generator/src/ScriptEditor.java
@@ -1,8 +1,16 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+/* File: ScriptEditor.java
+ * Author: Connor McGarty, cmcgarty@uab.edu
+ * Assignment: Project 7 EE333 Spring 2018
+ * Vers: 1.0.0 04/17/2018 csm - initial coding
  */
+
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 
 /**
@@ -11,4 +19,68 @@
  */
 public class ScriptEditor {
     
+    public static final String TAG_TITLE           = "_USRTITLE_";
+    public static final String TAG_AUTHORS         = "_AUTHORS_";
+    public static final String TAG_EXPERIMENT_DATE = "_EXPDATE_";
+    public static final String TAG_SUBMISSION_DATE = "_SUBDATE_";
+    public static final String TAG_COURSE_CODE     = "_COURSECODE_";
+    public static final String TAG_COURSE_NAME     = "_COURSENAME_";
+    public static final String TAG_DEPARTMENT      = "_DEPARTMENT_";
+    public static final String TAG_SCHOOL          = "_SCHOOL_";
+    public static final String TAG_TEAM            = "_TEAMMEMBERS_";
+    public static final String TAG_SECTIONS        = "_SECTIONTREE_";
+    
+    /* File where sed scripts will be written to eventually be executed on
+       a master template. */
+    private FileOutputStream scriptStream;
+    private UserDoc          userInfo;
+    private File             script;
+
+    
+    public ScriptEditor(UserDoc template) {
+        
+        try {
+            
+            script = new File("script.sed");
+            
+            if (!script.exists())
+                script.createNewFile();
+            
+            scriptStream = new FileOutputStream(script);
+            
+        } catch (IOException ex) {
+            System.out.println(ex);
+        }
+        
+        this.userInfo = template;
+        
+        
+    }
+    
+    
+    public String newCommand(String userField, String templateField) throws IOException {
+        String cmd;
+        
+        /* The sed substitution command  */
+        cmd = String.format("\'s/%s/%s/g\'\n", templateField, userField);
+        scriptStream.write(cmd.getBytes());
+        
+        
+        
+        // 
+        
+    }
+    
+    public void writeCommand(String cmd) {
+        
+    }
+    
+    
+    public void deleteScript() {
+        
+    }
+    
+    public void makeHeaders(HeadingTree headers) {
+        
+    }
 }
