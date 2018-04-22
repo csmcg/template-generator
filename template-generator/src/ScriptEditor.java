b5cd20e208ac6fa2b03b7edf1ad258a2692f17d7
Connor McGarty <cmcgarty@uab.edu>
2018-04-21 21:36:26 -0500

    added copyFile method

--- template-generator/src/ScriptEditor.java
+++ template-generator/src/ScriptEditor.java
@@ -6,9 +6,11 @@
 
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
+import java.nio.channels.FileChannel;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -104,6 +106,8 @@ public class ScriptEditor {
         String OS = System.getProperty("os.name");
         boolean isWindows;
         List<String> cmd = new ArrayList();
+        
+        
 
 
         isWindows = (OS.contains("windows") || OS.contains("Windows"));
@@ -112,12 +116,14 @@ public class ScriptEditor {
             cmd.add("cmd.exe");
             cmd.add("/c");
             cmd.add("sed");
+            cmd.add("-r");
             cmd.add("-f");
         }
         else {
             cmd.add("sh"); 
             cmd.add("-c");
             cmd.add("sed");
+            cmd.add("-r");
             cmd.add("-f");
         }
         
@@ -155,5 +161,36 @@ public class ScriptEditor {
         int exitCode = process.waitFor();
         //assert exitCode == 0; 
         */
+    }
+    
+    /**
+     * Taken from: https://stackoverflow.com/questions/106770/standard-concise-way-to-copy-a-file-in-java/115086#115086
+     * 
+     * 
+     * @param sourceFile
+     * @param destFile
+     * @throws IOException 
+     */
+    
+    public void copyFile(File sourceFile, File destFile) throws IOException {
+        if(!destFile.exists()) {
+            destFile.createNewFile();
+        }
+        
+        FileChannel source = null;
+        FileChannel destination = null;
+        
+        try {
+            source = new FileInputStream(sourceFile).getChannel();
+            destination = new FileOutputStream(destFile).getChannel();
+            destination.transferFrom(source, 0, source.size());
+        } finally {
+            if(source != null) {
+                source.close();
+            }
+            if(destination != null) {
+                destination.close();
+            }
+        }
     }
 }
