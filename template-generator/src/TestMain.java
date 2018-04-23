9b5789d7d35fdf8edec35b9b30d5dba6676bf86d
Connor McGarty <cmcgarty@uab.edu>
2018-04-23 14:43:05 -0500

    fixed "_SECTION" --> "_SECTION_" in FormalMasterTemplate.tex

--- template-generator/src/TestMain.java
+++ template-generator/src/TestMain.java
@@ -38,7 +38,8 @@ public class TestMain {
         } catch (FileAlreadyExistsException faeex) {
             
         }
-        
+
+        /* formal
         script.newCommand(ScriptEditor.TAG_TITLE, "Test Title");
         script.newCommand(ScriptEditor.TAG_COURSE_CODE, "EE123");
         script.newCommand(ScriptEditor.TAG_ASSIGNMENT, "Test Assignment");
@@ -47,9 +48,20 @@ public class TestMain {
         script.newCommand(ScriptEditor.TAG_DEPARTMENT, "Test Department");
         script.newCommand(ScriptEditor.TAG_SUBMISSION_DATE, "Test Sub Date");
         script.newCommand(ScriptEditor.TAG_EXPERIMENT_DATE, "Test Exp Date");
+        */
         
+        // informal
+        script.newCommand(ScriptEditor.TAG_MEMO_TO, "Dr. David Green");
+        script.newCommand(ScriptEditor.TAG_MEMO_FROM, "Team 6");
+        script.newCommand(ScriptEditor.TAG_MEMO_DATE, "23 April 2018");
+        script.newCommand(ScriptEditor.TAG_MEMO_SUBJECT, "This is a test");
+
         
-        Path userDoc = script.runScript(TEMPLATE.FORMAL, FORMAT.TEX, usrDocPath);
+        //Path userDoc = script.runScript(TEMPLATE.FORMAL, FORMAT.TEX, usrDocPath);
+        Path userDoc = script.runScript(TEMPLATE.INFORMAL, FORMAT.TEX, usrDocPath);
+        //Path userDoc = script.runScript(TEMPLATE.FORMAL, FORMAT.RTF, usrDocPath);
+        //Path userDoc = script.runScript(TEMPLATE.INFORMAL, FORMAT.RTF, usrDocPath);
+
         try {
             Files.deleteIfExists(usrDocPath);
             Files.copy(userDoc, usrDocPath, StandardCopyOption.COPY_ATTRIBUTES);
